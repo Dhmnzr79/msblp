34ca12af91b3bc823a11e12b8e309be7347434a3
Denis <denistoday@gmail.com>
2025-12-08 21:15:23 +0200

    08.12 - основные страницы готовы

--- header.php
+++ header.php
@@ -85,10 +85,10 @@
                                     <li><a href="<?php echo esc_url(home_url('/mission')); ?>">Миссия, Цели, Задачи</a></li>
                                     <li><a href="<?php echo esc_url(home_url('/audience')); ?>">Аудитория</a></li>
                                     <li><a href="<?php echo esc_url(home_url('/theory')); ?>">Теория и методика</a></li>
-                                    <li><a href="#">Интегральная онтопсихология</a></li>
+                                    <li><a href="<?php echo esc_url(psych_school_get_page_url_by_template('page-integral-ontopsychology.php')); ?>">Интегральная онтопсихология</a></li>
                                     <li><a href="<?php echo esc_url(home_url('/how-ontopsychology')); ?>">Как работает интегральная онтопсихология</a></li>
                                     <li><a href="<?php echo esc_url(home_url('/leader-address')); ?>">Обращение руководителя</a></li>
-                                    <li><a href="#">Принципы (Наши приоритеты)</a></li>
+                                    <li><a href="<?php echo esc_url(psych_school_get_page_url_by_template('page-principles.php')); ?>">Принципы (Наши приоритеты)</a></li>
                                     <li><a href="<?php echo esc_url(home_url('/founders')); ?>">Основатели</a></li>
                                     <li><a href="<?php echo esc_url(psych_school_get_page_url_by_template('page-prepodavateli.php')); ?>">Преподаватели</a></li>
                                     <li><a href="#">Сведения об образовательной организации</a></li>
@@ -171,8 +171,9 @@
                 <li><a href="<?php echo esc_url(home_url('/mission')); ?>">Миссия, Цели, Задачи</a></li>
                 <li><a href="<?php echo esc_url(home_url('/audience')); ?>">Аудитория</a></li>
                 <li><a href="<?php echo esc_url(home_url('/theory')); ?>">Теория и методика</a></li>
+                <li><a href="<?php echo esc_url(psych_school_get_page_url_by_template('page-integral-ontopsychology.php')); ?>">Интегральная онтопсихология</a></li>
                 <li><a href="<?php echo esc_url(home_url('/how-ontopsychology')); ?>">Как работает интегральная онтопсихология</a></li>
-                <li><a href="#">Принципы (Наши приоритеты)</a></li>
+                <li><a href="<?php echo esc_url(psych_school_get_page_url_by_template('page-principles.php')); ?>">Принципы (Наши приоритеты)</a></li>
                 <li><a href="<?php echo esc_url(home_url('/founders')); ?>">Основатели</a></li>
                 <li><a href="<?php echo esc_url(home_url('/leader-address')); ?>">Обращение руководителя</a></li>
                 <li><a href="<?php echo esc_url(psych_school_get_page_url_by_template('page-prepodavateli.php')); ?>">Преподаватели</a></li>
